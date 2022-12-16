311460179ce77cbf0349f9ef8e0bc73152052bd5
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-16 08:59:07 -0300

    Add server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    app = express(),
+    cors = require('cors');
+
+app.use(express.json());
+
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(cors());
+
+// middlewares
+app.use(morgan('dev'));
+
+// error handler
+app.use(function(err, req, res, next){
+    res.status(400).json({error: err, message: err.message});
+});
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
+});
