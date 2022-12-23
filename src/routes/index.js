c127509f2a3cd2af4966ff279fcc6782bf5d02f1
Marcos Volpi <marcos@marcosvolpi.com>
2022-12-23 11:08:44 -0300

    Feat (Customer) Add Validations on creation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,49 @@ const {
     customerControllerOb,
     cotizationControllerOb
 } = require('../dependencies/');
+const Joi = require('joi');
 
-router.put('/customer/', customerControllerOb.add);
+router.put('/customer/', addCustomer, customerControllerOb.add);
 router.post('/customer/login/', customerControllerOb.login);
 router.get('/cotizations/all/', cotizationControllerOb.list);
 router.post('/cotizations/add/', cotizationControllerOb.add);
 router.get('/cotizations/user/:order/:limit', cotizationControllerOb.listByUser);
+
+function addCustomer(req, res, next){
+    const schema = Joi.object({
+        password: Joi.string().min(8).max(20).required(),
+        first_name: Joi.string().max(50).required(),
+        last_name: Joi.string().max(50).required(),
+        username: Joi.string().max(50).required(),
+        favorite_fiat_coin: Joi.string().max(50).required(),
+    });
+    validateRequest(req, next, schema);
+}
+
+
+function validateRequest(req, next, schema) {
+    const options = {
+        abortEarly: false, // include all errors
+        allowUnknown: true, // ignore unknown props
+        stripUnknown: true // remove unknown props
+    };
+    let params;
+
+    if(Object.keys(req.body).length>0){
+        params=req.body;
+
+    }else{
+        params=req.params;
+    }
+
+    const { error, value } = schema.validate(params, options);
+
+    if (error) {
+        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
+    } else {
+        req.body = value;
+        next();
+    }
+}
 
 module.exports = router;
